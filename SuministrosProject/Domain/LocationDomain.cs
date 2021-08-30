db023262ef4535f57aa8f66e40968c9b140315db
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-08-30 13:50:17 -0600

    Domain de Localization

---
+++ SuministrosProject/Domain/LocationDomain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SuministrosProject.Models;
+
+namespace SuministrosProject.Domain
+{
+    public class LocationDomain
+    {
+        public string validarLocation(Localizacion localizacion)
+        {
+            var DescriptionIsNull = localizacion.descripcion == null;
+            if (DescriptionIsNull)
+            {
+                return "Por favor ingrese una descripción";
+            }
+            return null;
+        }
+    }
+}
