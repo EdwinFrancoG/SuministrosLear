83440abdcd26107f2c26d154c421d341bc9d3d14
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-08-30 10:58:40 -0600

    JS de Localización

---
+++ SuministrosProject/Content/JavaS/LocalizacionFuctions.js
@@ -0,0 +1,23 @@
+﻿function NewLocation() {
+    $.ajax(
+        {
+            type: 'GET',
+            url: '/Localizacions/Create',
+            success: function (result) {
+                $('#NewModalBody').html(result);
+                $('#NewModal').modal('show');
+            },
+            error: function (error) {
+                // si hay un error lanzara el mensaje de error
+                notificacioError('Error', 'Ocurrio un error desconocido, al comunicarse con el servidor');
+            }
+        });
+
+}
+
+$(function () {
+    $('#NewModal').on('shown.bs.modal', function (e) {
+        $('.focus').focus();
+    })
+});
+
