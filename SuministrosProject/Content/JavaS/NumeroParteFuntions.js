10ece29423b4419848ba6179b14a7e92fa065de1
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-08-30 14:50:20 -0600

    Add files via upload

---
+++ SuministrosProject/Content/JavaS/NumeroParteFuntions.js
@@ -0,0 +1,22 @@
+﻿function NewNumberPart() {
+    $.ajax(
+        {
+            type: 'GET',
+            url: '/NumeroParte/Create',
+            success: function (result) {
+                $('#NewModalBody').html(result);
+                $('#NewModal').modal('show');
+            },
+            error: function (error) {
+                // si hay un error lanzara el mensaje de error
+                notificacioError('Error', 'Ocurrio un error desconocido, al comunicarse con el servidor');
+            }
+        });
+
+}
+
+$(function () {
+    $('#NewModal').on('shown.bs.modal', function (e) {
+        $('.focus').focus();
+    })
+});
