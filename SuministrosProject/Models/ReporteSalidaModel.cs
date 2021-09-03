13039a02ac8bf8d4c8fb246f008fbee1df6843e3
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-03 07:31:52 -0600

    Clase de reporte de salidas

---
+++ SuministrosProject/Models/ReporteSalidaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuministrosProject.Models
+{
+    public class ReporteSalidaModel
+    {
+        public string serieSuministro { get; set; }
+        public string numeroParte { get; set; }
+        public string fechaSalida { get; set; }
+        public string equipo { get; set; }
+    }
+}
