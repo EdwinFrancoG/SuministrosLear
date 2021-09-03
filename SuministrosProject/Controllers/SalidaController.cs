fc823e9db3e183ca211efa4cef0566881d92ad3a
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-03 07:32:57 -0600

    Update SalidaController.cs

--- SuministrosProject/Controllers/SalidaController.cs
+++ SuministrosProject/Controllers/SalidaController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using SuministrosProject.Models;
 using SuministrosProject.AppServices;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace SuministrosProject.Controllers
 {
@@ -16,7 +19,7 @@ namespace SuministrosProject.Controllers
         // GET: Salida
         public async Task<ActionResult> Index()
         {
-            var salidas = db.Salida.Include(s => s.IdEquipoNaviation).Include(s => s.IdSuministroNavigation);          
+            var salidas = db.Salida.Include(s => s.IdEquipoNaviation).Include(s => s.IdSuministroNavigation);
             return View(await salidas.ToListAsync());
         }
 
@@ -41,6 +44,48 @@ namespace SuministrosProject.Controllers
             ViewBag.idEquipo = new SelectList(db.Equipo, "idEquipo", "equipo");
             ViewBag.IdSuministro = new SelectList(db.Suministro, "IdSuministro", "Serie");
             return View();
+        }
+
+        public ActionResult reporteSalidas()
+        {
+            var salidasReport = db.Salida.FromSqlRaw("Select idEquipo, COUNT(idEquipo) AS CantidadSuministros FROM salida GROUP BY(idEquipo)").ToListAsync();
+            return View(salidasReport);
+        }
+
+        //public Microsoft.AspNetCore.Mvc.ActionResult<List<ReporteSalidaModel>> ReportSalidas(string startDate, string endDate, string numeroParte)
+        public ActionResult ReportSalidas()
+        {
+            var d1 = "2021-08-27";
+            DateTime startDate = DateTime.Parse(d1);
+            DateTime endDate = DateTime.Now;
+
+            try
+            {
+                List<ReporteSalidaModel> ReporteSalidaSuministros = new List<ReporteSalidaModel>();
+                ReporteSalidaSuministros = (from s in db.Salida
+                                            join sum in db.Suministro on s.IdSuministro equals sum.IdSuministro
+                                            join np in db.NumeroParte on sum.IdNumeroParte equals np.IdNumeroParte
+                                            join e in db.Equipo on s.idEquipo equals e.idEquipo
+
+                                            where s.FechaSalida >= startDate && s.FechaSalida <= endDate
+                                            orderby s.FechaSalida
+
+                                            select new ReporteSalidaModel
+                                            {
+                                                serieSuministro = sum.Serie,
+                                                numeroParte = np.Descripcion,
+                                                fechaSalida = Convert.ToString(s.FechaSalida),
+                                                equipo = e.equipo
+                                            }).ToList();
+
+                return View(ReporteSalidaSuministros);
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         // POST: Salida/Create
