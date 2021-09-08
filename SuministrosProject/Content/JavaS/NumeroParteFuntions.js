dc94d63e9dfa9d9895abfb324466df0571468544
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-08 14:36:46 -0600

    Update NumeroParteFuntions.js

--- SuministrosProject/Content/JavaS/NumeroParteFuntions.js
+++ SuministrosProject/Content/JavaS/NumeroParteFuntions.js
@@ -1,4 +1,4 @@
-﻿function NewNumberPart() {
+function NewNumberPart() {
     $.ajax(
         {
             type: 'GET',
@@ -14,6 +14,42 @@
         });
 
 }
+
+
+function SaveNumberPart() {
+    var DescriptionNP = document.getElementById("idDescriptionNP").value;
+    var ModelNP = document.getElementById("ModelNP").value;
+    var BrandNP = document.getElementById("BrandNP").value;
+    var TypeNP = document.getElementById("TypeNP").value;
+    var ObservationNP = document.getElementById("ObservationNP").value;
+    $.ajax(
+        {
+            type: 'POST',
+            url: '/NumeroParte/create',
+            data: {
+                Descripcion: DescriptionNP,
+                Modelo: ModelNP,
+                Marca: BrandNP,
+                IdCategoria: TypeNP,
+                Observacion: ObservationNP
+            },
+            success: function (result) {
+                if (result == 'OK') {
+                    $('#BodyPositiveNP').html("Datos guardados con exito");
+                    $('#idAlertPositiveNP').modal('show');
+                }
+                else {
+                    $('#BodyNegative').html(result);
+                    $('#idAlertNegative').modal('show');
+                }
+            },
+            error: function (error) {
+                // si hay un error lanzara el mensaje de error
+                notificacioError('Error', 'Ocurrio un error desconocido, al comunicarse con el servidor');
+            }
+        });
+}
+
 
 $(function () {
     $('#NewModal').on('shown.bs.modal', function (e) {
