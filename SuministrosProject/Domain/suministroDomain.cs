c745fd6c646eb50232afc059f003f59aab8b43ce
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-08 07:50:58 -0600

    Update suministroDomain.cs

--- SuministrosProject/Domain/suministroDomain.cs
+++ SuministrosProject/Domain/suministroDomain.cs
@@ -1,4 +1,4 @@
-﻿using SuministrosProject.Models;
+using SuministrosProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,7 +29,7 @@ namespace SuministrosProject.Domain
             if (serieIsEmpty)
             {
                 return "Ingrese la serie del suministro";
-            }                       
+            }
 
             bool state = true;
             suministro.Estado = state;
@@ -40,13 +40,39 @@ namespace SuministrosProject.Domain
 
         public string validarEliminacion(int id)
         {
-            //var buscarExistencia = db.Stock.Where(s => s.IdSuministro == id).FirstOrDefault();
-            //bool existeEnExistencia = buscarExistencia != null;
-            //if (existeEnExistencia)
-            //{
-            //    return "No se puede eliminar porque aun hay existencia de este suministro";
-            //}
-            return null;
+            //variables para busqueda de suministro en listado de suministros y en el stock
+            var suministro = db.Suministro.Where(s => s.IdSuministro == id).FirstOrDefault();
+            var numeroParteSuministro = suministro.IdNumeroParte;
+            var numeroParteEnStock = db.Stock.Where(n => n.IdNumeroParte == numeroParteSuministro).FirstOrDefault();
+
+            //variables de stock para calculos
+            int salidas = Convert.ToInt32(numeroParteEnStock.Salidas);
+            int cantidadActual = Convert.ToInt32(numeroParteEnStock.CantidadActual);
+            int pendientes = Convert.ToInt32(numeroParteEnStock.Pendientes);
+            int stockIniscial = Convert.ToInt32(numeroParteEnStock.StockInicial);
+            int entradas = Convert.ToInt32(numeroParteEnStock.Entradas);
+            int total = Convert.ToInt32(numeroParteEnStock.Total);
+
+            //Calculos al elimininar un suministro
+            if (cantidadActual <= 0)
+            {
+                return "No hay suministros en stock de este numero de parte";
+            }
+            else
+            {
+                int salidasUpdate; int cantidadActualUpdate; int totalUpdate;
+
+                salidasUpdate = salidas + 1;
+                cantidadActualUpdate = (stockIniscial + entradas) - salidasUpdate;
+                totalUpdate = cantidadActualUpdate + pendientes;
+
+                numeroParteEnStock.Salidas = salidasUpdate;
+                numeroParteEnStock.CantidadActual = cantidadActualUpdate;
+                numeroParteEnStock.Total = totalUpdate;
+
+                db.SaveChangesAsync();
+                return null;
+            }
         }
     }
 }
