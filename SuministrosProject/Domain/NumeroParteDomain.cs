aa8f3316fbe483fb3bdafa5043c4becea236ed22
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-09 10:53:21 -0600

    Update NumeroParteDomain.cs

--- SuministrosProject/Domain/NumeroParteDomain.cs
+++ SuministrosProject/Domain/NumeroParteDomain.cs
@@ -1,11 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SuministrosProject.Models;
 
 namespace SuministrosProject.Domain
 {
     public class NumeroParteDomain
     {
+        public string validarInsercionNumeroParte(NumeroParte _numeroParte)
+        {
+            bool descripcionEstaVacio = _numeroParte.Descripcion == null;
+            if (descripcionEstaVacio)
+            {
+                return "Insert the part number description";
+            }
+
+            bool marcaEstaVacio = _numeroParte.Marca == null;
+            if (marcaEstaVacio)
+            {
+                return "Please, Insert the part number brand";
+            }
+
+            _numeroParte.Estado = true;
+            return null;
+        }
+
+
+        public string validarEliminacion()
+        {
+            return null;
+        }
     }
 }
