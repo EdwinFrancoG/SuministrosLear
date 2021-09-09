6912e618d9d577d63777dda631720e1d312c95a1
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-09 10:47:33 -0600

    Update NumeroParteFuntions.js

--- SuministrosProject/Content/JavaS/NumeroParteFuntions.js
+++ SuministrosProject/Content/JavaS/NumeroParteFuntions.js
@@ -9,7 +9,7 @@ function NewNumberPart() {
             },
             error: function (error) {
                 // si hay un error lanzara el mensaje de error
-                notificacioError('Error', 'Ocurrio un error desconocido, al comunicarse con el servidor');
+                notificacioError('Error', 'An unknown error occurred, communicating with the server');
             }
         });
 
@@ -35,7 +35,7 @@ function SaveNumberPart() {
             },
             success: function (result) {
                 if (result == 'OK') {
-                    $('#BodyPositiveNP').html("Datos guardados con exito");
+                    $('#BodyPositiveNP').html("Data saved successfully");
                     $('#idAlertPositiveNP').modal('show');
                 }
                 else {
@@ -45,7 +45,7 @@ function SaveNumberPart() {
             },
             error: function (error) {
                 // si hay un error lanzara el mensaje de error
-                notificacioError('Error', 'Ocurrio un error desconocido, al comunicarse con el servidor');
+                notificacioError('Error', 'An unknown error occurred, communicating with the server');
             }
         });
 }
@@ -56,3 +56,69 @@ $(function () {
         $('.focus').focus();
     })
 });
+
+
+function EditNumeroParte(_id) {
+    $.ajax(
+        {
+            type: 'GET',
+            url: '/NumeroParte/Edit',
+            data: { id: _id },
+            success: function (result) {
+                $('#NewModalBody').html(result);
+                $('#NewModal').modal('show');
+            },
+
+            error: function (error) {
+                // si hay un error lanzara el mensaje de error
+                notificacioError('Error', 'An unknown error occurred, communicating with the server');
+            }
+        });
+}
+
+
+
+//get de eliminar
+function GetDeleteNumeroParte(idNP) {
+    $.ajax(
+        {
+            type: 'GET',
+            url: '/NumeroParte/Delete',
+            data: {
+                id: idNP
+            },
+            success: function (result) {
+                $('#NewModalBody').html(result);
+                $('#NewModal').modal('show');
+            },
+            error: function (error) {
+                // si hay un error lanzara el mensaje de error
+                notificacioError('Error', 'An unknown error occurred, communicating with the server');
+            }
+        });
+
+}
+
+function postDeleteNumeroParte(_id) {
+    $.ajax(
+        {
+            type: 'POST',
+            url: '/NumeroParte/Delete',
+            data: { id: _id },
+            success: function (result) {
+                if (result == 'OK') {
+                    $('#BodyPositiveNP').html("Record Deleted");
+                    $('#idAlertPositiveNP').modal('show');
+                }
+                else {
+                    $('#BodyNegative').html(result);
+                    $('#idAlertNegative').modal('show');
+                }
+            },
+
+            error: function (error) {
+                // si hay un error lanzara el mensaje de error
+                notificacioError('Error', 'An unknown error occurred, communicating with the server');
+            }
+        });
+}
