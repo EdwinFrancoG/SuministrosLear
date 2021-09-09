dde3cfbf17e07662acdcfbcad50e4a4977ab534b
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-09 10:49:38 -0600

    Update NumeroParteController.cs

--- SuministrosProject/Controllers/NumeroParteController.cs
+++ SuministrosProject/Controllers/NumeroParteController.cs
@@ -5,33 +5,20 @@ using System.Web;
 using System.Web.Mvc;
 using SuministrosProject.Models;
 using System.Linq;
+using SuministrosProject.AppServices;
 
 namespace SuministrosProject.Controllers
 {
     public class NumeroParteController : Controller
     {
         private SuministrosContext db = new SuministrosContext();
+        public readonly NumeroParteAppSevices _numeroParteAppServices = new NumeroParteAppSevices();
 
         // GET: NumeroParte
         public async Task<ActionResult> Index()
         {
             var numeroPartes = db.NumeroParte.Include(n => n.IdCategoriaNavigation);
             return View(await numeroPartes.ToListAsync());
-        }
-
-        // GET: NumeroParte/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            NumeroParte numeroParte = await db.NumeroParte.FindAsync(id);
-            if (numeroParte == null)
-            {
-                return HttpNotFound();
-            }
-            return View(numeroParte);
         }
 
         // GET: NumeroParte/Create
@@ -45,17 +32,18 @@ namespace SuministrosProject.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public async Task<ActionResult> Create([Bind(Include = "IdNumeroParte,Descripcion,Modelo,Marca,IdCategoria,Observacion,Estado")] NumeroParte numeroParte)
+        public async Task<string> Create([Bind(Include = "IdNumeroParte,Descripcion,Modelo,Marca,IdCategoria,Observacion,Estado")] NumeroParte numeroParte)
         {
-            if (ModelState.IsValid)
+            var respuestaAppServices = await _numeroParteAppServices.ingresarNumeroParte(numeroParte);
+            bool errorEnRespuesta = respuestaAppServices != null;
+            if (errorEnRespuesta)
             {
-                db.NumeroParte.Add(numeroParte);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                return respuestaAppServices;
             }
-
-            ViewBag.IdCategoria = new SelectList(db.Categoria, "IdCategoria", "CategoriaDescripcion", numeroParte.IdCategoria);
-            return View(numeroParte);
+            else
+            {
+                return "OK";
+            }
         }
 
         // GET: NumeroParte/Edit/5
@@ -98,7 +86,7 @@ namespace SuministrosProject.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            NumeroParte numeroParte = await db.NumeroParte.FindAsync(id);
+            NumeroParte numeroParte = await db.NumeroParte.Include(t=>t.IdCategoriaNavigation).FirstOrDefaultAsync(n=>n.IdNumeroParte == id);
             if (numeroParte == null)
             {
                 return HttpNotFound();
@@ -108,13 +96,18 @@ namespace SuministrosProject.Controllers
 
         // POST: NumeroParte/Delete/5
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
+        public async Task<string> DeleteConfirmed(int id)
         {
-            NumeroParte numeroParte = await db.NumeroParte.FindAsync(id);
-            db.NumeroParte.Remove(numeroParte);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            var respuestaAppServices = await _numeroParteAppServices.EliminarNumeroParte(id);
+            bool errorEnRespuesta = respuestaAppServices != null;
+            if (errorEnRespuesta)
+            {
+                return respuestaAppServices;
+            }
+            else
+            {
+                return "OK";
+            }
         }
 
         protected override void Dispose(bool disposing)
