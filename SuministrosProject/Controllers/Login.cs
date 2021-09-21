d0a83b411d33d57702fd3a3b997de2f5ae0e211b
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-21 08:12:24 -0600

    Controlador login

---
+++ SuministrosProject/Controllers/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace SuministrosProject.Models
+{
+    public class Login
+    {
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
+}
