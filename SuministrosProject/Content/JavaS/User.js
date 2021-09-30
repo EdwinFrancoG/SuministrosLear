692b32d8fe990540c35f397269e6e20929be1ea9
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-09-30 10:59:41 -0600

    Add files via upload

---
+++ SuministrosProject/Content/JavaS/User.js
@@ -0,0 +1,16 @@
+﻿function NewUser() {
+    $.ajax(
+        {
+            type: 'GET',
+            url: '/Usuario/Create',
+            success: function (result) {
+                $('#NewModalBody').html(result);
+                $('#NewModal').modal('show');
+            },
+            error: function (error) {
+                // si hay un error lanzara el mensaje de error
+                notificacioError('Error', 'An unknown error occurred, communicating with the server');
+            }
+        });
+
+}
