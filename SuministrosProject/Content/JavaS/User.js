8f19141f8b13c1fcc713354a976d594e97b6bcce
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-10-01 14:45:00 -0600

    Update User.js

--- SuministrosProject/Content/JavaS/User.js
+++ SuministrosProject/Content/JavaS/User.js
@@ -1,4 +1,4 @@
-﻿function NewUser() {
+function NewUser() {
     $.ajax(
         {
             type: 'GET',
@@ -13,4 +13,40 @@
             }
         });
 
+}
+
+
+function SaveUser() {
+    var gafete = document.getElementById("idCreateBy").value;
+    var name = document.getElementById("idName").value;
+    var lastName = document.getElementById("idlastName").value;
+    var user = document.getElementById("User").value;
+    var Perfil = document.getElementById("idperfil").value;
+    $.ajax(
+        {
+            type: 'POST',
+            url: '/Usuario/create',
+            data: {
+                IdGafete: gafete,
+                Nombre: name,
+                Apellido: lastName,
+                usuario: user,
+                Estado: true,
+                IdPerfil: Perfil
+            },
+            success: function (result) {
+                if (result == 'OK') {
+                    $('#BodyPositiveU').html("Data saved successfully");
+                    $('#idAlertPositiveU').modal('show');
+                }
+                else {
+                    $('#BodyNegative').html(result);
+                    $('#idAlertNegative').modal('show');
+                }
+            },
+            error: function (error) {
+                // si hay un error lanzara el mensaje de error
+                notificacioError('Error', 'An unknown error occurred, communicating with the server');
+            }
+        });
 }
