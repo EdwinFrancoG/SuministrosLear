afdbb94b89e01d682107dd818e08d16d9ad7f00f
EdwinFrancoG <53808462+EdwinFrancoG@users.noreply.github.com>
2021-10-04 06:17:52 -0600

    Add files via upload

---
+++ SuministrosProject/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.Runtime.InteropServices;
+using SuministrosProject.Models;
+using System.Web.Mvc;
+using System.ComponentModel;
+
+namespace SuministrosProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        private SuministrosContext db = new SuministrosContext();
+        // GET: Login
+        public ActionResult loginGet()
+        {
+            return View();
+        }
+
+        public string Logear(string usuario, string password)
+        {
+       
+            using (DirectoryEntry entry = new DirectoryEntry())
+            {
+                entry.Username = usuario;
+                entry.Password = password;
+
+                DirectorySearcher directorySearcher = new DirectorySearcher(entry);
+
+                directorySearcher.Filter = "(objectclass=user)";
+
+                try
+                {
+                    if (directorySearcher.FindOne() != null)
+                    {
+                        if (db.Usuario.FirstOrDefault(u=>u.usuario == usuario) != null)
+                        {
+                            return "OK";
+                        }
+                        else
+                        {
+                            return "NofoundInDB";
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    return "NoEncontrado";
+                }
+                return "OK";
+            }
+
+        }
+    }
+}
